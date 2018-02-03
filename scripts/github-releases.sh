410e0ec175c72d36976c1fb15f7189d7bcf063a4
Rémi Jouannet <remi.jouannet@outscale.com>
2018-02-03 19:37:30 +0100

    add github-releases.sh

---
+++ scripts/github-releases.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set +e
+
+version=$(git describe --abbrev=0 --tags)
+
+which github-release || echo 'please install the tool github-releases'
+
+github-release info \
+    --user remijouannet \
+    --repo packer-osc-plugins \
+    --tag $version
+
+if [ $? != 0 ]
+then
+    echo "the release doesn't exist"
+    github-release release \
+        --user remijouannet \
+        --repo packer-osc-plugins \
+        --tag $version \
+        --pre-release \
+        --name "$version-hyper-alpha-yolo-experimental" \
+        --description "risks of explosion" || echo "failed to create release for $version"
+fi
+
+cd pkg/
+
+rm -f *.zip
+ 
+ls | while read binary
+do
+    echo "zipping $binary"
+    zip -9 -r $binary.zip $binary
+    echo "upload $binary"
+    github-release upload \
+        --user remijouannet \
+        --name "$binary.zip" \
+        --repo packer-osc-plugins \
+        --file "$binary.zip" \
+        --replace \
+        --tag $version || echo "failed to upload $binary"
+done
